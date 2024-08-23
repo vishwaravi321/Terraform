8f22e2a939e47da2852431fd72f08be0bbaa6429
Vishwa <91792810+vishwaravi321@users.noreply.github.com>
2024-08-23 16:57:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,19 +19,3 @@ resource "hcloud_server" "my_server" {
   ssh_keys = [var.ssh_key]
 }
 
-output "Name" {
-  value = hcloud_server.my_server.name
-}
-
-output "ip" {
-  value = hcloud_server.my_server.ipv4_address
-}
-
-output "Server-Type" {
-  value = hcloud_server.my_server.server_type
-}
-
-output "Image" {
-  value = hcloud_server.my_server.image
-}
-
